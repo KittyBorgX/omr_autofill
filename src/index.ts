5beb04086740cbf358d469d9f297688ee7dd8e7a
Krishna Ramasimha <krishnaramasimha12@gmail.com>
2025-05-15 10:27:40 +0530

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,107 @@
+import * as fs from 'fs';
+import { PDFDocument, PDFFont, PDFPage, rgb, StandardFonts } from 'pdf-lib';
+import { Config, StudentData } from './types';
+
+
+export async function fillOMRSingle(
+    page: PDFPage,
+    student: StudentData,
+    config: Config,
+    font: PDFFont
+) {
+    const drawText = (
+        text: string,
+        x: number,
+        y: number,
+        size: number = 12
+    ) => {
+        page.drawText(text, {
+            x,
+            y,
+            font,
+            size,
+            color: rgb(0, 0, 0)
+        });
+    };
+
+    const drawBubble = (x: number, y: number, radius: number = 5) => {
+        page.drawCircle({ x, y, size: radius, color: rgb(0, 0, 0) });
+    };
+
+    for (const [fieldName, field] of Object.entries(config)) {
+        const value = (student as any)[fieldName];
+        if (!value) continue;
+
+        if (field.type === 'bubble-grid') {
+            for (let i = 0; i < value.length; i++) {
+                const digit = value[i];
+                const x = field.x[i.toString()];
+                const y = field.y[digit];
+                if (x !== undefined && y !== undefined) {
+                    drawBubble(x, y, field.radius ?? 5);
+
+                    if (field.text) {
+                        drawText(
+                            value[i],
+                            x + (field.text.xOffset ?? 0),
+                            field.text.y,
+                            field.text.fontSize ?? 10
+                        );
+                    }
+                }
+            }
+        }
+
+        if (field.type === 'text') {
+            drawText(value, field.x, field.y, field.fontSize ?? 14);
+        }
+    }
+}
+
+export async function generateOMR(
+    templateBytes: Uint8Array,
+    students: StudentData[],
+    config: Config
+): Promise<Uint8Array> {
+    const templatePdf = await PDFDocument.load(templateBytes);
+    const mergedPdf = await PDFDocument.create();
+    const font = await mergedPdf.embedFont(StandardFonts.Courier);
+
+    const configErrors = validateConfig(config);
+    // TODO: Better and prettier error handling
+    if (configErrors.length) throw new Error(configErrors.join('\n'));
+
+    for (const student of students) {
+        const [templatePage] = await mergedPdf.copyPages(templatePdf, [0]);
+        mergedPdf.addPage(templatePage);
+
+        await fillOMRSingle(templatePage, student, config, font);
+    }
+
+    return await mergedPdf.save();
+}
+
+function validateConfig(_config: Config): string[] {
+    // TODO:
+    // Implement checks in the config to ensure certain fields like `x` and `y` are always present
+    // and check the lengths of all fields to ensure that out of bounds indices are never an issue.
+    return []
+}
+
+async function main() {
+    const templateBytes = await fs.promises.readFile('omr_template.pdf');
+    const config = await fs.promises.readFile('coordinates.json', 'utf8')
+    const config_json = JSON.parse(config);
+    const data = {
+        "uid": "0000000000",
+        "name": "Student 1"
+    }
+    const students: StudentData[] = [data];
+    const filled = await generateOMR(templateBytes, students, config_json);
+    await fs.promises.writeFile('filled_output.pdf', filled).then(() => {
+        console.log('OMR written at: filled_output.pdf');
+    });
+}
+
+main()
+
---
+++ src/types.ts
@@ -0,0 +1,30 @@
+export type BubbleGridField = {
+  type: 'bubble-grid';
+  length: number;
+  radius?: number;
+  x: Record<string, number>; // index → x
+  y: Record<string, number>; // digit → y
+  text?: {
+    y: number;
+    fontSize?: number;
+    xOffset?: number;
+  };
+};
+
+export type TextField = {
+  type: 'text';
+  x: number;
+  y: number;
+  fontSize?: number;
+};
+
+export type FieldConfig = BubbleGridField | TextField;
+
+export type Config = {
+  [fieldName: string]: FieldConfig;
+};
+
+export type StudentData = {
+    uid: string;
+    name: string;
+};
